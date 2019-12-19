3931606db174fdc2174917763b34ea5f74d94a0f
neerajaggarwal07 <neeraj.aggarwal07@gmail.com>
2019-12-19 23:33:26 +0530

    added storybook addons (#5)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/vue';
+import { configure,addDecorator } from '@storybook/vue';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs, object } from '@storybook/addon-knobs/vue';
+import { withConsole } from '@storybook/addon-console'
+
+addDecorator(withKnobs)
+addDecorator(withA11y)
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 function loadStories() {
   const req = require.context('../components', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
--- components/atoms/Button/Button.stories.js
+++ components/atoms/Button/Button.stories.js
@@ -1,16 +1,34 @@
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
+import Vue from 'vue';
+import { storiesOf, addParameters} from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
 import Button from './Button'
 storiesOf('Atom - Button', module)
+.addParameters({
+  info: {
+    inline: true, header: false
+  },
+})
   .add('Default', () => ({
     components: { Button },
-    template: '<Button />'
-  }))
+    template: '<Button @click="action" />',
+    methods: { action: action('clicked : Default') }
+  }),{
+    notes: 'Example of Default Button Variant',
+  })
   .add('Rounded', () => ({
     components: { Button },
-    template: '<Button  class="rounded"/>'
-  }))
+    template: '<Button  class="rounded" @click="action" />',
+    methods: { action: action('clicked') }
+  }),{
+    notes: 'Example of Rounded Button Variant',
+  })
   .add('Disabled', () => ({
     components: { Button },
-    template: '<Button  class="disabled"/>'
-  }))
+    template: '<Button  class="disabled" @click="action" />',
+    methods: { action: action('clicked') },
+
+
+  }),{
+    notes: 'Example of Disabled Button Variant',
+  })
